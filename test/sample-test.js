7a11578a21521bd2bc0ee66a3038fa2017a53c34
quantroc <ducquannguyen28699@gmail.com>
2022-05-24 10:53:14 +0700

    add .

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,45 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe ("Vesting Contract", function () {
+  let Vesting, vesting, QuanToken, quanToken, owner, addr1, addr2, addr3;
+  const _DEFAULT_ISSUE_TOKEN = 1000000000000000000000;
+  const decimals = ethers.BigNumber.from(10).pow(18);
+  beforeEach(async function(){
+      [owner, addr1, addr2, addr3, ...addr4] = await ethers.getSigners();
+      let utcTimestamp = new Date().getTime();
+      utcTimestamp = 1498140000;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+      QuanToken = await ethers.getContractFactory("QuanToken");
+      quanToken = await QuanToken.deploy();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+      Vesting = await ethers.getContractFactory("Vesting");
+      vesting = await Vesting.deploy(
+          quanToken.address, 
+          20, 
+          utcTimestamp , 
+          5, 
+          2, 
+          2,  
+          ethers.BigNumber.from(1000).mul(decimals)
+      );
+      console.log("timestamp: ",utcTimestamp);
+  });
+  
+  it("Check Claim Function", async function(){
+      //Whitelist
+      await vesting.addWhiteList(addr1.address, 1000);
+      await vesting.addWhiteList(addr2.address, 3500);
+      await vesting.addWhiteList(addr3.address, 2300);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+      //Issuing Token for Owner
+      await quanToken.issueToken();
+      
+      //Approve for vesting to using owner's token
+      const totalTokens = await vesting.getTotalTokens();
+      await quanToken.approve(vesting.address, totalTokens);
+      await vesting.fundVesting(totalTokens);
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+      //Claim
   });
 });
